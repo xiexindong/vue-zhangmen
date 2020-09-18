9ec8a821d6514b44326397a44d73edce8ce270e8
xindong.xie <xindong.xie@zhangmen.com>
2020-09-18 19:44:24 +0800

    22222

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,24 @@
+/* eslint-disable */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
 const routes = [{
     path: '/',
-    name: 'Home',
-    component: Home
-}, {
-    path: '/about',
     name: 'About',
+    component: About
+}, {
+    path: '/Home',
+    name: 'Home',
     component: () =>
-        import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    import ( /* webpackChunkName: "home" */ '../views/Home.vue')
+}, {
+    path: '/Admin',
+    name: 'Admin',
+    component: () =>
+    import ( /* webpackChunkName: "Admin" */ '../views/Admin.vue')
 }]
 
 const router = new VueRouter({
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,74 @@
 <template>
   <div class="about">
-    <el-row>
-      <el-col :span="24">
-        <el-button >222</el-button>
-      </el-col>
-    </el-row>
+    <div class="block">
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page.sync="currentPage3"
+      :page-size="1"
+      layout="prev, pager, next, jumper"
+      :total="1000">
+    </el-pagination>
+  </div>
   </div>
 </template>
+<script>
+export default {
+  /* eslint-disable */
+    name: 'admin',
+    data () {
+      return {
+         currentPage3: 1,
+         tableData: [{
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄'
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄'
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄'
+          }]
+      }
+    }, 
+    
+   methods: {
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
+      }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
